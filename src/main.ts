e17deb94de5237ba921f1620499b22247c0f176b
Kamil Essekkat <kamil@essekkat.pl>
2023-03-06 21:17:23 +0100

    Package

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { program } from "./cli";
 import { testSelect } from "./select";
+import { renderWordTemplate } from "./templates";
 
 program.parse();
 const options = program.opts();
 console.log(options);
 
 testSelect();
+renderWordTemplate({
+  word: "foo",
+  usage: "Bar",
+});
