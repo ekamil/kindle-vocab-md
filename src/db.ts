866a1c9ab02513e3f9f004675151f3fed7eaf683
Kamil Essekkat <kamil@essekkat.pl>
2023-03-25 17:09:17 +0100

    Remove unused code

--- src/db.ts
+++ src/db.ts
@@ -1,9 +1,5 @@
 import { Database } from "sqlite3";
 
-export const openDB = (path = "vocab.db") => {
-  return new Database(path);
-};
-
 interface Lookup {
   id: string;
   word_key: string;
@@ -23,12 +19,12 @@ interface Word {
 
 export class Repository {
   private readonly db: Database;
-  constructor(path: string = "vocab.db") {
+  constructor(path = "vocab.db") {
     this.db = new Database(path);
   }
 
   async findWord(partial: string): Promise<Word> {
-    return new Promise<Word>((resolve, reject) => {
+    return new Promise<Word>((resolve) => {
       this.db.get(
         "SELECT * from words where stem like ?",
         [partial],
