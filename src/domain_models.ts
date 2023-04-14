5a61f67473068cc751e59e4943a961565d3f551f
Kamil Essekkat <kamil@essekkat.pl>
2023-04-14 09:49:38 +0200

    Move model creation to service

--- src/domain_models.ts
+++ src/domain_models.ts
@@ -1,8 +1,8 @@
-import type { BookT, BookKey, LookupT, WordT } from "./db_models";
+import type { BookT, BookKey, LookupT, WordT, WordKey } from "./db_models";
 
 // intermediary between db_models and templates
 export class Book {
-  readonly id: BookKey;
+  readonly book_key: BookKey;
   readonly title: string;
   readonly authors: string;
   readonly asin: string;
@@ -14,7 +14,7 @@ export class Book {
   }
 
   constructor(book_info: BookT) {
-    this.id = book_info.id;
+    this.book_key = book_info.id;
     this.title = book_info.title;
     this.authors = book_info.authors;
     this.asin = book_info.asin;
@@ -22,11 +22,20 @@ export class Book {
   }
 }
 
-interface Lookup {
+export class Lookup {
+  word_key: WordKey;
   usage: string;
   book: Book;
   pos: string;
   date: Date;
+
+  constructor(l: LookupT, book: Book) {
+    this.word_key = l.word_key;
+    this.usage = l.usage;
+    this.pos = l.pos;
+    this.date = new Date(l.timestamp);
+    this.book = book;
+  }
 }
 
 export class LookedUpWord {
@@ -51,26 +60,16 @@ export class LookedUpWord {
     return from_lookup != null ? from_lookup : long_time_ago;
   }
 
-  constructor(
-    word: WordT,
-    lookups: LookupT[],
-    books: ReadonlyMap<BookKey, Book>,
-  ) {
+  constructor(word: WordT, lookups: Lookup[]) {
     this.word_key = word.id;
     this.word = word.word;
     this.stem = word.stem;
     this.lookups = lookups
       .map((l) => {
-        const book_info = books.get(l.book_key);
-        if (book_info === undefined) {
-          throw `missing book for lookup ${l}`;
+        if (l.word_key != this.word_key) {
+          throw `invalid lookup ${l} for word ${this.word_key}`;
         }
-        return {
-          usage: l.usage,
-          pos: l.pos,
-          date: new Date(l.timestamp),
-          book: book_info,
-        };
+        return l;
       })
       .map((l) => {
         // markdown highlighting in sentence
