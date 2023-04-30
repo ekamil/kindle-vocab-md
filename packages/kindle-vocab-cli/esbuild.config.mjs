58f7bb81f173a1164c1ddb7f98416b87fe2766fa
Kamil Essekkat <kamil@essekkat.pl>
2023-04-30 17:54:20 +0200

    ESBuild - packages have to be external for bindings (sqlite) to work

--- packages/kindle-vocab-cli/esbuild.config.mjs
+++ packages/kindle-vocab-cli/esbuild.config.mjs
@@ -1,13 +1,16 @@
 import esbuild from "esbuild";
 import process from "process";
+import metadata from "./package.json" assert { type: "json" };
 
 
 esbuild.build({
     entryPoints: ["src/main.ts"],
     outfile: "dist/main.js",
     bundle: true,
+    external: Object.keys(metadata.dependencies).concat(Object.keys(metadata.devDependencies)),
+    packages: "external",
     format: "esm",
-    target: "es2016",
+    target: "esnext",
     platform: "node",
     sourcemap: true,
     minify: false,
